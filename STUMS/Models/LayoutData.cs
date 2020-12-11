705ba13ee8b2b3eece7260bd597aa1f760eaa775
LiaoZhimin <876100193@qq.com>
2020-12-11 11:24:29 +0800

    框架搭建

---
+++ STUMS/Models/LayoutData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STUMS.Models
+{
+    public class LayoutData
+    {
+        /// <summary>
+        /// 是否已经登录
+        /// </summary>
+        public bool IsLogin { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 用户代码
+        /// </summary>
+        public string UserCode { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string PPPP { get; set; }
+        /// <summary>
+        /// 邮件
+        /// </summary>
+        public string Email { get; set; }
+
+    }
+}
---
+++ STUMS_Models/SS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUMS_Models
+{
+    public class Lgn_M
+    {
+        public string UUUU { get; set; }
+        public string PPPP { get; set; }
+    }
+}
