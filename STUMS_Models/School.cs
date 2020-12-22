5bbe5351bab92dd1e0a025d4527b06a3e87c2ec4
LiaoZhimin <876100193@qq.com>
2020-12-22 13:42:23 +0800

    添加model 的 EF

---
+++ STUMS_Models/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUMS_Models
+{
+    public class School
+    {
+        public int ID { get; set; }
+        public string SchoolName { get; set; }
+        public string Addr { get; set; }
+        public double Longitude { get; set; }
+        public double latitude { get; set; }
+    }
+}
