b3037545a1b250400ca62e4b8e3b5586bc581861
LiaoZhimin <876100193@qq.com>
2021-01-22 16:25:53 +0800

    优化登录操作

--- STUMS/Controllers/LgnController.cs
+++ STUMS/Controllers/LgnController.cs
@@ -9,7 +9,11 @@ namespace STUMS.Controllers
 {
     public class LgnController : ApiController
     {
-
+        /// <summary>
+        /// 登录，账号UUUU,密码PPPP校验
+        /// </summary>
+        /// <param name="mm">UUUU账号，PPPP密码</param>
+        /// <returns></returns>
         public string Post([FromBody]STUMS_Models.Lgn_M mm)
         {
             return "OK";
